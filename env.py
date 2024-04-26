2808a526ed930ebbbf283b70c8925d63dcdcd325
DavidePaglieri <paglieridavide@gmail.com>
2024-04-26 11:44:55 +0100

    feat: integrated new observation

--- env.py
+++ env.py
@@ -1,22 +1,52 @@
 # Super minimized version of what diff_history does
 
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+# from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from nle_language_wrapper import NLELanguageWrapper
 import difflib
-from prompt_builder import DiffPromptBuilder, ConcatPromptBuilder, nle_text_obs
+from prompt_builder import DiffPromptBuilder, ConcatPromptBuilder
 from progress import Progress
+
 
 # what call this?
 class NLEExtendedLanguageWrapper(NLELanguageWrapper):
-    def __init__(self, env, *, max_history=None, max_length=None, use_diff_history=False, action_token="<|action|>", obs_token="<|observation|>"):
+    def __init__(
+        self,
+        env,
+        *,
+        max_history=None,
+        max_length=None,
+        use_diff_history=False,
+        action_token="<|action|>",
+        obs_token="<|observation|>",
+    ):
         super().__init__(env)
-        action_names = [action_strs[0] for action, action_strs in self.all_nle_action_map.items() if action in env.actions]
+        action_names = [
+            action_strs[0]
+            for action, action_strs in self.all_nle_action_map.items()
+            if action in env.actions
+        ]
         prefix = "You are an agent playing NetHack. Predict the next keypresses.\n\n"
-        prefix += f"Output only one of the following actions:\n\n" + ", ".join(action_names) + "\n\n"
+        prefix += (
+            f"Output only one of the following actions:\n\n"
+            + ", ".join(action_names)
+            + "\n\n"
+        )
         if use_diff_history:
-            self._prompt_builder = DiffPromptBuilder(max_history=max_history, max_length=max_length, prefix=prefix, action_token=action_token, obs_token=obs_token)
+            self._prompt_builder = DiffPromptBuilder(
+                max_history=max_history,
+                max_length=max_length,
+                prefix=prefix,
+                action_token=action_token,
+                obs_token=obs_token,
+            )
         else:
-            self._prompt_builder = ConcatPromptBuilder(max_history=max_history, max_length=max_length, prefix=prefix, action_token=action_token, obs_token=obs_token)
+            self._prompt_builder = ConcatPromptBuilder(
+                max_history=max_history,
+                max_length=max_length,
+                prefix=prefix,
+                action_token=action_token,
+                obs_token=obs_token,
+            )
         self._progress = Progress()
 
     # override
@@ -27,23 +57,21 @@ class NLEExtendedLanguageWrapper(NLELanguageWrapper):
 
     # override
     def post_reset(self, nle_obsv):
-        obsv = nle_text_obs(super().post_step(nle_obsv))
+        obsv = super().post_step(nle_obsv)
         self._prompt_builder.append_observation(obsv)
         return self._prompt_builder.get_prompt()
-    
+
     # override
     def pre_step(self, action):
         self._prompt_builder.append_action(action)
         return super().pre_step(action)
-    
+
     # override
     def post_step(self, nle_obsv):
         obsv = super().post_step(nle_obsv)
-        self._progress.update(obsv["text_message"], obsv["text_blstats"])
-        obsv = nle_text_obs(obsv)
         self._prompt_builder.append_observation(obsv)
         return self._prompt_builder.get_prompt()
-    
+
     # override
     def step(self, action):
         obs, reward, done, info = super().step(action)
@@ -52,9 +80,10 @@ class NLEExtendedLanguageWrapper(NLELanguageWrapper):
         info["achievements"] = self._progress.get_achievements()
         return obs, reward, done, info
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from nle.env import tasks
-    
+
     base_env = tasks.NetHackChallenge(
         **dict(
             # savedir="./experiment_outputs/dummy_ttyrec",
@@ -75,18 +104,19 @@ if __name__ == '__main__':
             # save_ttyrec_every=1,
         )
     )
-    
-    tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
-    model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it", device_map="auto")
-    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_length=100000, return_full_text=False)
 
-    env = NLEExtendedLanguageWrapper(base_env, use_diff_history=True)
-    
+    # tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
+    # model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it", device_map="auto")
+    # pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_length=100000, return_full_text=False)
+
+    env = NLEExtendedLanguageWrapper(base_env, use_diff_history=False)
+
     obs = env.reset()
-    
+
     for n_steps in range(100000):
         print(obs)
-        action = pipe(obs, max_length=1000)[0]['generated_text']
+        # action = pipe(obs, max_length=1000)[0]["generated_text"]
         obs, reward, done, info = env.step("north")
         print(info)
+        # sleep(0.1)
         quit()
--- prompt_builder.py
+++ prompt_builder.py
@@ -1,25 +1,34 @@
 from nle_language_wrapper import NLELanguageWrapper
 import difflib
 
+
 class PromptBuilder(object):
-    def __init__(self, *, max_history=None, max_length=None, prefix="", action_token="<|action|>", obs_token="<|observation|>"):
-        self._prefix  = prefix
+    def __init__(
+        self,
+        *,
+        max_history=None,
+        max_length=None,
+        prefix="",
+        action_token="<|action|>",
+        obs_token="<|observation|>",
+    ):
+        self._prefix = prefix
         self._max_history = max_history
         self._max_length = max_length
         self._obs_history = []
         self._action_history = []
         self._action_token = action_token
         self._obs_token = obs_token
-        
+
     def append_action(self, action):
         self._action_history.append(action)
-        
+
     def append_observation(self, obs):
         self._obs_history.append(obs)
-        
+
     def get_prompt(self):
         return self._prefix + self._format()
-    
+
     def reset(self):
         self._obs_history = []
         self._action_history = []
@@ -27,21 +36,28 @@ class PromptBuilder(object):
     def _format(self):
         n_steps = len(self._action_history)
         start_idx = min(self._max_history or (n_steps + 1), n_steps + 1)
-        
+
         if self._max_length is None:
-            return self._format_history(self._obs_history[-start_idx-1:], self._action_history[-start_idx:])
-        
-        for i in reversed(range(1, start_idx+1)):
-            text = self._format_history(self._obs_history[-i-1:], self._action_history[-i:])
-            num_tokens = len(text.encode('utf-8')) # Ideal world, we know exactly how many tokens this string is, but estimate using num bytes
+            return self._format_history(
+                self._obs_history[-start_idx - 1 :], self._action_history[-start_idx:]
+            )
+
+        for i in reversed(range(1, start_idx + 1)):
+            text = self._format_history(
+                self._obs_history[-i - 1 :], self._action_history[-i:]
+            )
+            num_tokens = len(
+                text.encode("utf-8")
+            )  # Ideal world, we know exactly how many tokens this string is, but estimate using num bytes
             if num_tokens <= self._max_length:
                 return text
-            
+
         raise ValueError("Unable to generate context that fits within max_length.")
-        
+
     def _format_history(self, obs_history, action_history):
         raise NotImplementedError
-    
+
+
 class ConcatPromptBuilder(PromptBuilder):
     def _format_history(self, obs_history, action_history):
         text = ""
@@ -49,30 +65,24 @@ class ConcatPromptBuilder(PromptBuilder):
             text += f"{self._obs_token}" + obs + f"{self._action_token}" + action
         text += f"{self._obs_token}" + obs_history[-1] + f"{self._action_token}"
         return text
-    
+
+
 class DiffPromptBuilder(PromptBuilder):
     def _format_history(self, obs_history, action_history):
         text = f"{self._obs_token}" + obs_history[0]
-        for action, (prev_obs, obs) in zip(action_history, zip(obs_history[:-1], obs_history[1:])):
+        for action, (prev_obs, obs) in zip(
+            action_history, zip(obs_history[:-1], obs_history[1:])
+        ):
             prev_obs = prev_obs.strip().splitlines()
             obs = obs.strip().splitlines()
             obs = "\n".join(difflib.unified_diff(prev_obs, obs, n=0, lineterm=""))
             text += f"{self._action_token}" + action + f"{self._obs_token}" + obs
         text += f"{self._action_token}"
         return text
-    
+
+
 def get_default_prefix():
     # action_names = [action_strs[0] for action, action_strs in self.all_nle_action_map.items() if action in env.actions]
     prefix = "You are an agent playing NetHack. Predict the next keypresses.\n\n"
     return prefix
     # prefix += f"Output only one of the following actions:\n\n" + ", ".join(action_names) + "\n\n"
-
-def nle_text_obs(text_obsv):
-    key_name_pairs = [
-        ("text_blstats", "statistics"),
-        ("text_glyphs", "glyphs"),
-        ("text_message", "message"),
-        ("text_inventory", "inventory"),
-        ("text_cursor", "cursor"),
-    ]
-    return "\n".join([f"{name}[\n{text_obsv[key]}\n]" for key, name in key_name_pairs])
