3930d0b8dac74aa033a4ae8d1878de47e01608e0
DavidePaglieri <paglieridavide@gmail.com>
2024-06-15 18:29:50 +0100

    chore: add to init

--- fmrl/prompt_builder/__init__.py
+++ fmrl/prompt_builder/__init__.py
@@ -3,3 +3,4 @@ from .builders.default import DefaultPromptBuilder
 from .builders.concat import ConcatHistoryPromptBuilder
 from .builders.diff import DiffHistoryPromptBuilder
 from .builders.chat import ChatPromptBuilder
+from .builders.human import HumanHistoryPromptBuilder
