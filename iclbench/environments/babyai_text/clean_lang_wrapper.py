4c27062b76b424f3035099d9b228cc018a9526ba
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-09-25 19:05:40 +0200

    fix seeding for babyai

--- iclbench/environments/babyai_text/clean_lang_wrapper.py
+++ iclbench/environments/babyai_text/clean_lang_wrapper.py
@@ -1,6 +1,4 @@
-import babyai_text
 import gym
-from gym import Wrapper
 from PIL import Image
 
 BABYAI_ACTION_SPACE = [
@@ -13,13 +11,8 @@ BABYAI_ACTION_SPACE = [
 ]
 
 
-class BabyAITextCleanLangWrapper(Wrapper):
-    def __init__(self, task, vlm=False, **kwargs):
-        base_task, goal = task.split("/")
-        while 1:
-            env = gym.make(base_task, **kwargs)
-            if env.env.action_kinds[0].replace(" ", "_") == goal:
-                break
+class BabyAITextCleanLangWrapper(gym.Wrapper):
+    def __init__(self, env, vlm=False, **kwargs):
         super().__init__(env)
         self.language_action_space = BABYAI_ACTION_SPACE[:]
         self._mission = None
