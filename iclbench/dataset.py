497bbd4f6256cbb9cd22fa88f809548516f13801
Bartłomiej Cupiał <bartlomiej.cupial@gmail.com>
2024-09-25 19:13:23 +0200

    current version of the ICL dataset

--- iclbench/dataset.py
+++ iclbench/dataset.py
@@ -1,6 +1,13 @@
 import copy
 import pickle
+import re
 from pathlib import Path
+
+from iclbench.environments.textworld import global_textworld_context
+
+
+def natural_sort_key(s):
+    return [int(c) if c.isdigit() else c.lower() for c in re.split(r"(\d+)", str(s))]
 
 
 class InContextDataset:
@@ -9,18 +16,40 @@ class InContextDataset:
         self.env_name = env_name
         self.original_cwd = original_cwd
 
-    def demo_path(self, i, task):
+    def icl_episodes(self, task):
         demos_dir = Path(self.original_cwd) / self.config.eval.icl_dataset / self.env_name / task
-        demo_path = list(demos_dir.iterdir())[i]
+        return list(sorted(demos_dir.iterdir(), key=natural_sort_key))
+
+    def check_seed(self, demo_path):
+        return int(demo_path.stem.split("seed_")[1])
+
+    def demo_path(self, i, task):
+        demo_path = self.icl_episodes(task)[i]
+
+        if self.env_name == "textworld":
+            # avoid the case where we put the solution into the context
+            textworld_context = global_textworld_context(
+                tasks=self.config.tasks.textworld_tasks, **self.config.envs.textworld_kwargs
+            )
+            next_seed = textworld_context.count[task]
+            demo_seed = self.check_seed(demo_path)
+            if next_seed == demo_seed:
+                demo_path = self.icl_episodes(task)[i + 1]
 
         return demo_path
 
     def load_incontext_config(self, i, task):
-        # TODO: if needed we should use the same character as in nethack
-        demo_path = self.demo_path(i, task)
-        seed = int(demo_path.stem.split("_")[1])
         episode_config = copy.deepcopy(self.config)
+        demo_path = self.demo_path(i, task)
+        seed = self.check_seed(demo_path)
+
         episode_config.envs.env_kwargs.seed = seed
+
+        if self.env_name == "nle" or self.env_name == "minihack":
+            episode_config.envs[f"{self.env_name}_kwargs"].skip_more = True
+
+        if self.env_name == "crafter":
+            episode_config.envs.crafter_kwargs.seed = seed
 
         return episode_config
 
