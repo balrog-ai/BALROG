7560db21f341b4806bb9db2144a130316c430ae0
Bartek Cupiał <bartlomiej.cupial@gmail.com>
2024-11-05 14:55:10 +0100

    add docstring to wrapper

--- balrog/environments/wrappers/multiple_episodes.py
+++ balrog/environments/wrappers/multiple_episodes.py
@@ -4,9 +4,17 @@ import gym
 
 
 class MultiEpisodeWrapper(gym.Wrapper):
-    def __init__(self, env, num_episodes):
+    def __init__(self, env, num_episodes, aggregate_function=max):
+        """
+        A Gym environment wrapper to run multiple episodes and aggregate statistics.
+        Args:
+            env (gym.Env): The environment to wrap.
+            num_episodes (int): The number of episodes to run.
+            aggregate_function (callable, optional): The function to aggregate statistics across episodes. Defaults to max. Could be mean, min, etc.
+        """
         super().__init__(env)
         self.num_episodes = num_episodes
+        self.aggregate_function = aggregate_function
         self.current_episode = 0
         self.total_stats = defaultdict(list)
         self.max_steps = self.num_episodes * self.env.max_steps
@@ -32,9 +40,9 @@ class MultiEpisodeWrapper(gym.Wrapper):
 
     def get_stats(self):
         """
-        returns stats for max progression
+        returns stats for aggregate_function progression
         """
-        best_stats = max(
+        best_stats = self.aggregate_function(
             (stats for stats in self.total_stats),
             key=lambda stats: stats["progression"],
             default=None,
